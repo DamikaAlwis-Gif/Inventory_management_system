806400b5802c5ab9180af782ee6ebc726ef289fd
Oshan <oshansandeep630@gmail.com>
2023-09-04 13:11:41 +0530

    changed db.js

--- backend/dataBase/db.js
+++ backend/dataBase/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql";
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "damika",
+  password: "root",
   database: "inventorydbv2",
 });
 
