ad3362a75e0a47fe51e514d89651a3cff37db8fe
Dimuthu Amaraweera <120401856+DimuthuA@users.noreply.github.com>
2023-09-11 23:19:02 +0530

    The new UI for the Login Page

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-// import Button from "@mui/material/Button";
-import  Alert from '@mui/material/Alert';
 import { validateProperty , validate } from "../Validation/LoginValidation";
-import userImage from "../../Images/user.png";
-import institutionLogo from "../../Images/InstitutionLogo.svg"; 
+import loginImage from "../../Images/LoginImage.jpg";
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+
+import Typography from '@mui/material/Typography';
 
 
 const Login = () => {
@@ -19,19 +21,17 @@ const Login = () => {
   axios.defaults.withCredentials = true;
   const [errors, setErrors] = useState({});
 
-   
   const handleChange = (e) => {
-    
-    
     const errorslist = { ...errors };
     const error =validateProperty(e.target);
     
     if (error) errorslist[e.target.name] = error;
-    else delete errorslist[e.target.name]; 
+    else delete errorslist[e.target.name];
+
     setErrors(errorslist);
-    setValue({ ...value, [e.target.name]: e.target.value });
-    
+    setValue({ ...value, [e.target.name]: e.target.value }); 
   };
+
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -39,6 +39,7 @@ const Login = () => {
     const errors = validate(value);
     setErrors(errors || {});
     if (errors) return;
+
     try {
       const res = await axios.post("http://localhost:8800/auth/login", value);
       //console.log(res.data);
@@ -53,88 +54,106 @@ const Login = () => {
     }
   };
   console.log(value)
+  
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center g-0">
-        <div className="card  shadow border-2" style={{ maxWidth: "700px" }}>
-          <div
-            className="card-header text-center"
-            style={{ backgroundColor: "#007BFF", color: "white" }}
-          >
-            <h5 className="card-title" style={{ fontSize: "30px" }}>
-              Login
-            </h5>
-          </div>
+    <Container
+      maxWidth="xl"
+      sx={{
+        display: 'flex',
+        height: '100vh',
+        width: '75%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingBottom: '30px',
 
-          <div className="row g-0">
-            <div className="col-md-4">
-              <div className="custom-logo">
-                <img src={institutionLogo} alt="Logo" />
-              </div>
-              <div className="custom-name" style={{ lineHeight: "0.8" }}>
-                <span className="first-word">Wisdom</span>
-                <br />
-                <span className="second-word" style={{ fontSize: "0.7em" }}>
-                  Education
-                </span>
-              </div>
-            </div>
-            <div className="col-md-8">
-              <div className="card-body ">
-                <form onSubmit={(e) => handleSubmit(e)}>
-                  <div className="form-floating mb-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="username"
-                      placeholder="Enter username"
-                      name="user_name"
-                      value={value.user_name}
-                      onChange={(e) => handleChange(e)}
-                    />
-                    <label htmlFor="username">Username</label>
-                    {errors.user_name && (
-                      <Alert severity="warning">{errors.user_name}</Alert>
-                    )}
-                  </div>
-                  <div className="form-floating mb-3">
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="password"
-                      placeholder="Enter password"
-                      name="password"
-                      value={value.password}
-                      onChange={(e) => handleChange(e)}
-                    />
-                    <label htmlFor="password">Password</label>
-                    {errors.password && (
-                      <Alert severity="warning">{errors.password}</Alert>
-                    )}
-                  </div>
-                  {/* <button type="submit" className="btn btn-primary btn-sm">
-                  Login
-                </button> */}
-                  <button className="btn btn-primary btn" type="submit">
-                    Login
-                  </button>
-
-                  <Link to="/register" className="btn btn-link my-2 ">
-                    Create Account
-                  </Link>
-                </form>
-              </div>
-            </div>
-          </div>
-          {/* <div
-            className="card-header text-center"
-            style={{ fontWeight: "bolder", fontSize: "30px" }}
-          >
-            Login
-          </div> */}
-        </div>
-      </div>
-    </div>
+        '@media (max-width:1400px)': {width: '80%',},
+        '@media (max-width:1300px)': {width: '85%',},
+        '@media (max-width:1200px)': {width: '90%',},
+        '@media (max-width:1100px)': {width: '95%',},
+        '@media (max-width:1000px)': {width: '100%',},
+        '@media (max-width:900px)': {
+          width: '60%',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        },
+        '@media (max-width:750px)': {
+          width: '70%',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        },
+        '@media (max-width:500px)': {
+          width: '80%',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'space-around',
+        },
+      }}
+    >
+    
+      <Grid container alignItems="center" justifyContent="center" rowSpacing={3}>
+        <Grid item container sx={12} md={7} alignItems="center" justifyContent="center">
+          <img src={loginImage} alt="Image" style={{
+                maxWidth: '100%',
+                // maxHeight: '70vh',
+          }}/>
+        </Grid>
+        <Grid item container sx={12} md={5} alignItems="center" justifyContent="center">
+        <form noValidate onSubmit={(e) => handleSubmit(e)}>
+          <Grid container rowSpacing={4} justifyContent="center" alignItems="center" sx={{ maxWidth: '380px', minWidth: '260px'}}>
+            <Grid item xs={12} sx={{marginTop: '1rem', marginBottom: '0.5rem'}}>
+              <Typography
+                variant="h6"
+                gutterBottom
+                align="center"
+                style={{color: '#444444'}}>
+                  Welcome to the Inventory MIS
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="filled-basic"
+                label="Username"
+                variant="outlined"
+                fullWidth
+                type="text"
+                autoComplete="off"
+                name="user_name"
+                value={value.user_name}
+                onChange={(e) => handleChange(e)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                id="filled-basic"
+                label="Password"
+                variant="outlined"
+                fullWidth
+                type="password"
+                name="password"
+                value={value.password}
+                onChange={(e) => handleChange(e)}
+              />
+            </Grid>
+            <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'space-between', marginTop: '1rem' }}>
+          <Button onClick={() => navigate("/dashboard")} type="button" variant="text" color="primary" sx={{
+            borderRadius: '20px',
+            height: '40px',
+            textTransform: 'capitalize',
+          }}>Create Account</Button>
+          <Button type="submit" variant="contained" color="primary"  sx={{
+            borderRadius: '20px',
+            height: '40px',
+            textTransform: 'capitalize',            
+          }}>Log in</Button>
+        </Grid>
+          </Grid>
+        </form>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
