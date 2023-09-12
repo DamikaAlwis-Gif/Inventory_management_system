257c715059f73325f4216801db0fc78439daef65
DamikaAlwis <102840577+DamikaAlwis@users.noreply.github.com>
2023-09-12 17:07:27 +0530

    pagination error handled

--- client/src/components/utils/paginate.js
+++ client/src/components/utils/paginate.js
@@ -1,7 +1,15 @@
 import _ from "lodash";
 
 export function paginate(items, pageNumber, pageSize) {
-  const startIndex = (pageNumber - 1) * pageSize;
+  console.log("items", items);
+  console.log("pageNumber", pageNumber);
+  console.log("pageSize", pageSize);
+  
+  
+  let startIndex = (pageNumber - 1) * pageSize;
+  if(items.length < startIndex) {
+  startIndex = 0;
+  } 
   // convert items array to lodash wrapper object
   // slice the array from startIndex to startIndex + pageSize
   // convert lodash wrapper object to regular array
