2796cdb0b0402f01c9ca833285f80aaa34c64a26
Oshan Amarasooriya <118419101+oshanAmarasuriya@users.noreply.github.com>
2023-10-29 14:35:46 +0530

    Update db.js

--- backend/dataBase/db.js
+++ backend/dataBase/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql";
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root@inventory",
-  database: "inventorydbv2",
+  host: "mysql://u0iykdfonc4z1spz:pEX4MLxgMCsOtHEjeiyZ@bjobkmjrqevxml3klckh-mysql.services.clever-cloud.com:3306/bjobkmjrqevxml3klckh",
+  user: "u0iykdfonc4z1spz",
+  password: "pEX4MLxgMCsOtHEjeiyZ",
+  database: "bjobkmjrqevxml3klckh",
   timezone: 'UTC'
 });
 
