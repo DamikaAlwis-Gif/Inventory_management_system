7ce178f4962231f0044efc4a083f69fd22e26efa
Oshan Amarasooriya <118419101+oshanAmarasuriya@users.noreply.github.com>
2023-10-29 14:51:07 +0530

    Update db.js

--- backend/dataBase/db.js
+++ backend/dataBase/db.js
@@ -1,7 +1,7 @@
 import mysql from "mysql";
 
 const db = mysql.createConnection({
-  host: "mysql://u0iykdfonc4z1spz:pEX4MLxgMCsOtHEjeiyZ@bjobkmjrqevxml3klckh-mysql.services.clever-cloud.com:3306/bjobkmjrqevxml3klckh",
+  host: "mysql://u0iykdfonc4z1spz:pEX4MLxgMCsOtHEjeiyZ@bjobkmjrqevxml3klckh-mysql.services.clever-cloud.com:3306/bjobkmjrqevxml3klckh:3306",
   user: "u0iykdfonc4z1spz",
   password: "pEX4MLxgMCsOtHEjeiyZ",
   database: "bjobkmjrqevxml3klckh",
